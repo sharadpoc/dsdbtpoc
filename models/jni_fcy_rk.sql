82cd03e312a05bfe7606ab040bce7db2f61df568
sharadpoc <sharad.ing.poc@gmail.com>
2025-03-10 14:21:48 +0100

    Add files via upload

---
+++ models/jni_fcy_rk.sql
@@ -0,0 +1,37 @@
+
+{{config(
+  materialized='ephemeral'
+)}}
+
+WITH jni_fcy_rk AS (
+SELECT
+            		mov_SRT_EWM_AR_X_CL_RTG_R.RTG_ST_TP_CL_CD AS mov_srt_ewm_ar_x_cl_rtg_r_rtg_st_tp_cl_cd,
+            	mov_SRT_EWM_AR_X_CL_RTG_R.SRC_DL AS mov_srt_ewm_ar_x_cl_rtg_r_src_dl,
+            	mov_SRT_EWM_AR_X_CL_RTG_R.AR_ID AS mov_srt_ewm_ar_x_cl_rtg_r_ar_id,
+            	mov_SRT_EWM_AR_X_CL_RTG_R.MSTR_SRC_STM_CD AS mov_srt_ewm_ar_x_cl_rtg_r_mstr_src_stm_cd,
+            	mov_SRT_EWM_AR_X_CL_RTG_R.MSTR_SRC_STM_KEY AS mov_srt_ewm_ar_x_cl_rtg_r_mstr_src_stm_key,
+            	mov_SRT_EWM_AR_X_CL_RTG_R.VLD_FROM_TMS AS mov_srt_ewm_ar_x_cl_rtg_r_vld_from_tms,
+            	mov_SRT_EWM_AR_X_CL_RTG_R.VLD_TO_TMS AS mov_srt_ewm_ar_x_cl_rtg_r_vld_to_tms,
+            	mov_SRT_EWM_AR_X_CL_RTG_R.CL_CD AS mov_srt_ewm_ar_x_cl_rtg_r_cl_cd,
+            	mov_SRT_EWM_AR_X_CL_RTG_R.EFF_DT AS mov_srt_ewm_ar_x_cl_rtg_r_eff_dt,
+            	mov_SRT_EWM_AR_X_CL_RTG_R.INR_RTG_MODL_CL_CD AS mov_srt_ewm_ar_x_cl_rtg_r_inr_rtg_modl_cl_cd,
+            	mov_SRT_EWM_FCY_AR.FCY_RK AS mov_srt_ewm_fcy_ar_fcy_rk,
+            	mov_SRT_EWM_FCY_AR.SRC_DL AS mov_srt_ewm_fcy_ar_src_dl,
+            	mov_SRT_EWM_FCY_AR.AR_ID AS mov_srt_ewm_fcy_ar_ar_id
+        FROM
+            (
+                    SELECT
+                         *
+                    FROM
+                        {{ ref('mov_srt_ewm_ar_x_cl_rtg_r') }} AS mov_SRT_EWM_AR_X_CL_RTG_R
+                    UNION DISTINCT
+                    DISTINCT
+                    SELECT
+                         *
+                    FROM
+                        {{ ref('mov_srt_ewm_fcy_ar') }} AS mov_SRT_EWM_FCY_AR
+                ) 
+)
+
+SELECT * FROM jni_fcy_rk
+
---
+++ models/ora_read_ewm_ar_fcy_ar_v.sql
@@ -0,0 +1,21 @@
+
+{{config(
+  materialized='ephemeral'
+)}}
+
+WITH ora_read_ewm_ar_fcy_ar_v AS (
+SELECT
+        	SRC_DL,
+        	AR_ID,
+        	CONCAT(CONCAT(MSTR_SRC_STM_CD, '|'), MSTR_SRC_STM_KEY) AS fcy_rk
+    FROM
+        {{ source('xg_ps_rdb_dm_mpscr_database.xg_rdb_dm_schema_mpscr', 'ewm_ar_fcy_ar_v') }}
+    WHERE
+        VLD_FROM_TMS <= PARSE_TIMESTAMP
+        AND PARSE_TIMESTAMP < VLD_TO_TMS
+        AND SRC_DL='{{ var(' xg_pm_src_dl ')}}
+
+)
+
+SELECT * FROM ora_read_ewm_ar_fcy_ar_v
+
