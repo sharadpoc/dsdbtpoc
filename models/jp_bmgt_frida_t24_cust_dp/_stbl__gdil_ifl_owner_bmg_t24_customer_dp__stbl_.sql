311884b1b70472a6b8a2e2b9db62aa8633eea6c2
sharadpoc <sas2py@email.com>
2025-03-15 17:49:29 +0000

    a

--- models/jp_bmgt_frida_t24_cust_dp/_stbl__gdil_ifl_owner_bmg_t24_customer_dp__stbl_.sql
+++ models/jp_bmgt_frida_t24_cust_dp/_stbl__gdil_ifl_owner_bmg_t24_customer_dp__stbl_.sql
@@ -1,16 +1,12 @@
 
 {{config(
-  materialized='ephemeral'
+  materialized='table',
+  pre_hook="""
+  DELETE FROM GDIL_IFL_OWNER
+        WHERE ODS_BUSINESS_DATE >= '{{var('cb_pm_strt_busdate')}}'
+            AND ODS_BUSINESS_DATE <= '{{var('cb_pm_end_busdate')}}' 
+  """
 )}}
 
-WITH _stbl__gdil_ifl_owner_bmg_t24_customer_dp__stbl_ AS (
-_ATSQLB_
-        DELETE FROM
-            GDIL_IFL_OWNER_{{ ref('bmg_t24_customer_dp') }}
-        WHERE
-            ODS_BUSINESS_DATE >= '{{var("cb_pm_strt_busdate")}}'
-            AND ODS_BUSINESS_DATE <= '{{var("cb_pm_end_busdate")}}' 
-)
-
-SELECT * FROM _stbl__gdil_ifl_owner_bmg_t24_customer_dp__stbl_
+SELECT 1 AS dummy_column
 
