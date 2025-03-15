95b3645ed0aa6ecac6475b982d8ec253ed11b6f9
sharad.ing.poc@gmail.com <sharad.ing.poc@gmail.com>
2025-03-15 21:49:50 +0000

    removed where clauses

--- models/ora_read_ewm_ar_x_ip_r_m2.sql
+++ models/ora_read_ewm_ar_x_ip_r_m2.sql
@@ -11,16 +11,7 @@ SELECT
             	RANK
         FROM
             {{ source('DM_MPSCR', 'EWM_AR_X_IP_R') }}
-        WHERE
-            VLD_FROM_TMS <= PARSE_TIMESTAMP(
-                    '%Y%m%d%H%M%S',
-                    "{{var('xg_pm_selection_date')}}{{var('xg_pm_business_tms')}}"
-                )
-            AND PARSE_TIMESTAMP(
-                    '%Y%m%d%H%M%S',
-                    "{{var('xg_pm_selection_date')}}{{var('xg_pm_business_tms')}}"
-                ) < VLD_TO_TMS
-            AND SRC_DL='{{var("xg_pm_src_dl")}}' 
+
 )
 
 SELECT * FROM ora_read_ewm_ar_x_ip_r_m2
--- models/ora_read_ewm_ip_alt_identn_vortex.sql
+++ models/ora_read_ewm_ip_alt_identn_vortex.sql
@@ -10,17 +10,7 @@ SELECT
             	IP_ID_NM AS CST_VORTEX_ID
         FROM
             {{ source('DM_MPSCR', 'EWM_IP_ALT_IDENTN_M') }}
-        WHERE
-            VLD_FROM_TMS <= PARSE_TIMESTAMP(
-                    '%Y%m%d%H%M%S',
-                    "{{var('xg_pm_selection_date')}}{{var('xg_pm_business_tms')}}"
-                )
-            AND PARSE_TIMESTAMP(
-                    '%Y%m%d%H%M%S',
-                    "{{var('xg_pm_selection_date')}}{{var('xg_pm_business_tms')}}"
-                ) < VLD_TO_TMS
-            AND SRC_DL='{{var("xg_pm_src_dl")}}'
-            AND IP_IDENTN_TP_CL_CD='LCL_VORTEX_ID' 
+
 )
 
 SELECT * FROM ora_read_ewm_ip_alt_identn_vortex
