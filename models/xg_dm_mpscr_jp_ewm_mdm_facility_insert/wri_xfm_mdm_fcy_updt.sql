3389fdc915fdcf23cb88d1b6ae806e31d297a618
Faiz Founain Md <faiz.ingpoc@gmail.com>
2025-03-16 13:05:50 +0000

    16032025

--- models/xg_dm_mpscr_jp_ewm_mdm_facility_insert/wri_xfm_mdm_fcy_updt.sql
+++ models/xg_dm_mpscr_jp_ewm_mdm_facility_insert/wri_xfm_mdm_fcy_updt.sql
@@ -3,6 +3,28 @@
   materialized='table'
 )}}
 
+WITH wri_xfm_mdm_fcy_updt AS (
+SELECT
+    MOV_JNO_FACILITY.TGT_FCY_ID AS FCY_ID,
+    MOV_JNO_FACILITY.TGT_AR_ID AS AR_ID,
+    MOV_JNO_FACILITY.TGT_DATE_FROM AS DATE_FROM,
+    CASE
+        WHEN SVACTION = "D" THEN PARSE_TIMESTAMP("%Y%m%d", "{{var('xg_pm_selection_date')}}")
+        ELSE MOV_JNO_FACILITY.DATE_FROM
+    END AS DATE_TO,
+    PARSE_TIMESTAMP("%Y%m%d", "{{var('xg_pm_selection_date')}}") AS SYS_INRT_TMS,
+    MOV_JNO_FACILITY.TGT_SRC_ID AS SRC_ID,
+    MOV_JNO_FACILITY.TGT_SRC_DL AS SRC_DL,
+    CASE
+        WHEN SVACTION = "D" THEN "1"
+        ELSE "0"
+    END AS DEL_IN_SRC_STM_F
+FROM
+    {{ ref('mov_jno_facility') }} AS MOV_JNO_FACILITY
+WHERE
+    SVACTION = "U"
+    OR SVACTION = "D"
+)
 
-SELECT 1 as  dummy
+SELECT * FROM wri_xfm_mdm_fcy_updt
 
