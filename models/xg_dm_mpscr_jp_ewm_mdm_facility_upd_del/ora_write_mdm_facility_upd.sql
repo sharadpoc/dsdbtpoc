784d5f80491b57f5c0b068b3bd4f7fbc1cd9023f
Shashank Reddy Yakkanti <shashank.ingpoc@gmail.com>
2025-03-18 06:14:09 +0000

    facility_upd

--- models/xg_dm_mpscr_jp_ewm_mdm_facility_upd_del/ora_write_mdm_facility_upd.sql
+++ models/xg_dm_mpscr_jp_ewm_mdm_facility_upd_del/ora_write_mdm_facility_upd.sql
@@ -1,1 +1,11 @@
-select 1 as dummy
+{{ config(
+    materialized='table',
+    post_hook="CALL `cognizant-pocngdap-01.DM_MPSCR.merge_target_table`(
+            '{{ var('xg_pm_tgttbl') }}', 
+            '{{ var('xg_pm_tgttbl_update') }}', 
+            '{{ var('xg_pm_selection_date') }}', 
+            '{{ var('xg_pm_src_dl') }}'
+        )
+    "
+) }}
+SELECT 1 AS dummy_column
